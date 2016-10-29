d4614d725cbd749ccadbf591b9a0d0dbc3e7dcc7
SeverinetsDenis <severinetsdenis@gmail.com>
2016-10-29 15:13:58 +0200

    Some changes of the Spring annotation

--- src/main/java/com/journaldev/spring/service/EmployeeService.java
+++ src/main/java/com/journaldev/spring/service/EmployeeService.java
@@ -4,11 +4,12 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.journaldev.hibernate.data.Employee;
 
-@Component
+@Service
 public class EmployeeService {
 	@Autowired
 	private SessionFactory sessionFactory;
